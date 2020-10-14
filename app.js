aa898a072cd763eebcc7552a9a5133ce1cd03045
decaro-drew <55863884+decaro-drew@users.noreply.github.com>
2020-10-13 22:29:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var db = mysql.createConnection({
     password: 'commpantry',
     database: 'community_pantry'
 });
-
+//res.redirect to send to pages
 db.connect(function(error){
     if(error){
         console.log("Error while connecting to Database");
@@ -29,6 +29,7 @@ app.get("/", function(req, res){
 
 app.post("/login", function(req, res){
     db.query("SELECT username, pWord FROM user", function(error, rows, fields){
+        var success = false;
         if(error){
             console.log(error);
         }else{
@@ -37,6 +38,7 @@ app.post("/login", function(req, res){
                    console.log("username match");
                    if(rows[i].pWord == req.body.pword){
                        console.log("password match");
+                       success = true;
                        res.send("Successful login");
                    }else{
                        console.log("incorrect passowrd");
@@ -44,9 +46,39 @@ app.post("/login", function(req, res){
                    }
                }
            }
-           res.send("No such user");
+           if(!success)
+                res.send("No such user");
         }
     });
+});
+app.post("/createAccount", function(req, res){
+    var valid = true;
+    if(req.body.uName.length < 6){
+        res.send("Username must be at least 6 characters");
+        valid = false;
+    }
+    if(req.body.email.indexOf('@') == -1 || req.body.email.length == 0){
+        res.send("Invalid email address")
+        valid = false;
+    }
+    if(req.body.pWord.length < 6){
+        res.send("Password must be at least 6 characters");
+        valid = false;
+    }
+    if(req.body.cPword != req.body.pWord){
+        res.send("Passwords are not equal")
+        valid = false;
+    }
+    if(valid){
+        db.query("Insert into user (username, email, pWord) VALUES ('"+req.body.uName+"','"+req.body.email+"','"+req.body.pWord+"')",function(err, result){   
+            if(err){
+                res.send("Username already exists");
+            }
+            else{
+                res.send("Account created");
+            }
+        });
+    }
 });
 
 app.listen(3000, function(){
